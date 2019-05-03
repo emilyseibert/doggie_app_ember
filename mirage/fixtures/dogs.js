eb50a2bdf0fea96f42564f2afe18c3e0ccd5665d
Emily <emseibert@gmail.com>
2019-05-02 18:49:48 -0600

    add dog images

--- mirage/fixtures/dogs.js
+++ mirage/fixtures/dogs.js
@@ -1,5 +1,5 @@
 export default [
-    {id: 1, name: 'Penny', picture: 'blah'},
-    {id: 2, name: 'Buddy', picture: 'blah'},
-    {id: 3, name: 'Moose', picture: 'blah'},
+    {id: 1, name: 'Penny', picture: 'https://www.akc.org/wp-content/themes/akc/component-library/assets/img/welcome.jpg'},
+    {id: 2, name: 'Buddy', picture: 'https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/09/dog-landing-hero-sm.jpg?bust=1536935086'},
+    {id: 3, name: 'Moose', picture: 'https://amp.businessinsider.com/images/5ab514477708e97acc0f0cc9-750-562.jpg'},
 ];
