9a56d5eb9f68db3a4c0732cf14b21160cab9cd72
Emily <emseibert@gmail.com>
2019-05-02 18:09:40 -0600

    select new value

--- app/components/game-component.js
+++ app/components/game-component.js
@@ -4,17 +4,32 @@ import Component from '@ember/component';
 export default Component.extend({
     currentQuestion: null,
     currentDogIndex: 0,
+    selectedDog: null, // type Dog,
+    score: 0,
     questions: [],
     
     didRender() {
-        // access to dogs
-        // current dog
-        // index of question
-
+        // init game
         if (this.questions.length === 0) {
-            // generate list
+            // generate question
             this.set('questions', this.get('dogs'));
+            // get first q
             this.set('currentQuestion', this.get('questions').firstObject);
+        }
+    },
+
+    actions: {
+        // submit is called in q cmpt, param is dog in question cmpt, updates score, updates game, changes question
+        submit(dog) {
+            console.log(dog);
+            console.log('submitted');
+            // update score
+
+            this.set('currentDogIndex', this.get('currentDogIndex')+ 1);
+            this.set('currentQuestion', this.get('questions').objectAt(this.get('currentDogIndex')));
+        
+            // end of game (run out of quesitions)
+            // call end of game function
         }
     }
 });
--- app/components/question-component.js
+++ app/components/question-component.js
@@ -1,5 +1,19 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-    
+    // selected dog 
+    // options for select are params for the component
+    // onsubmit calls game submit(dog)
+    selectedDog: null, // type Dog
+
+    actions: {
+        selectDog(dog) {
+            this.set('selectedDog', dog);
+        },
+
+        submit(event) {
+            console.log('event', event);
+            this.submit(event); // call parent submit function (param)
+        }
+    }
 });
